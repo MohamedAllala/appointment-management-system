277cdf7ec094322ebe4059dabc68b6e39ea3892f
Mohamed Allala (mka) <mohamedkhayreddine.allala1@gmail.com>
2024-11-11 12:44:08 +0100

    Add login page

---
+++ Frontend/appoint-mgt-syst/src/components/users/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <form class="row">
+        <div class="col-12 mb-3">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter email">
+            </div>
+        </div>
+        <div class="col-12 mb-3">
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter password">
+            </div>
+        </div>
+
+        <div class="col-12 text-center mb-3">
+            <button type="submit" class="btn btn-primary">Log in</button>
+        </div>
+    </form>
+</template>
+<script lang="ts" setup>
+</script>
+<style lang="scss"></style>
--- Frontend/appoint-mgt-syst/src/pages/LoginPage.vue
+++ Frontend/appoint-mgt-syst/src/pages/LoginPage.vue
@@ -4,7 +4,11 @@
             <div class="login-page__left">
             </div>
             <div class="login-page__right">
-                <register></register>
+                <register v-if="!showLogin"></register>
+                <login v-else></login>
+                <div v-if="!showLogin" class="login-page__signin">
+                    <a href="#" @click.prevent="goToLogin"> Already have an account ? sign in here </a>
+                </div>
             </div>
         </div>
       
@@ -12,7 +16,15 @@
   </template> 
   
   <script setup lang="ts">
-  import Register from '../components/users/Register.vue';    
+  import Register from '../components/users/Register.vue';
+  import Login from '../components/users/Login.vue';
+import { ref } from 'vue';
+
+  const showLogin = ref(false);
+  const goToLogin = () => {
+    showLogin.value = true;
+  }
+
   </script>    
   
   <style lang="scss">
@@ -27,23 +39,30 @@
       align-items: center;
       height: 100%;
       width: 100%;
-      padding-left: 35px;
-      padding-right: 35px;
+      padding: 25px;
+      border: 1px solid var(--bs-gray-200);
+      border-radius: 15px;
+      background-color:  var(--bs-gray-100);
+      margin-top: 25px;
       #{$self}__left {
         width: 25%;
         background-color: #fff;
         position: relative;
         margin-right: 2px;
-        height: 70vh;
+        height: 100%;
         background-color: rgba(79,183,131,1);
         padding: 5px;
+        border-top-left-radius: 15px;
+        border-bottom-left-radius: 15px;
       }
       #{$self}__right  {
         width: 75%;
         background-color: #fff;
         position: relative;
-        height: 70vh;
+        height: 100%;
         padding: 5px;
+        border-top-right-radius: 15px;
+        border-bottom-right-radius: 15px;
       }
     }
    
